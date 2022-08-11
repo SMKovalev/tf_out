dcb718dfceed59f44d9b98f20a7cb9e6a7a409ec
Sergey Kovalev <ksm638@yandex.ru>
2022-08-11 17:23:17 +0000

    Create TF_OUT files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,4 @@
-provider "yandex"  {
+provider "yandex" {
  token     = var.yc_token
  cloud_id  = var.yc_cloud_id
  folder_id = var.yc_folder_id
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,6 @@ output "internal_ip_address_vm1" {
   value = yandex_compute_instance.vm1.network_interface.0.ip_address
 }
 
-output "external_ip_address_vm1"  {
+output "external_ip_address_vm1" {
   value = yandex_compute_instance.vm1.network_interface.0.nat_ip_address
 }
